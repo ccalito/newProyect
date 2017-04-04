6a9a1c2779f75110c45ef555c25d82525f65223c
ccalito <ccalito@bytesw.com>
2017-04-04 00:02:11 -0600

    se modifico

--- src/app/components/elementOperation.component.ts
+++ src/app/components/elementOperation.component.ts
@@ -7,15 +7,13 @@ import {FormComponent} from './formulaForm.component';
 selector: 'element-operation-tag',
   templateUrl: '../views/element-operation.view.html',
   styleUrls: ['../../assets/popup-form.css'],
-  providers: [FormComponent]
 
 })
 
 export class ElementOperationComponent{
     
     public elementOperation:ElementOperation = new ElementOperation(null,null,null,null,null);
-    constructor(private _formComponent : FormComponent
-    ){
+    constructor(){
     }
 
 
